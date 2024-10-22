b95ed921b3661f2cc275dec21cd07fe4477d3cf1
mattJsonar <matt.gill@imperva.com>
2024-10-21 17:21:59 -0700

    update secret manager acc tests

--- dsfhub/resource_secret_manager_test.go
+++ dsfhub/resource_secret_manager_test.go
@@ -2,36 +2,34 @@ package dsfhub
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"testing"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-	"log"
-	"testing"
 )
 
-const secretManagerResourceName = "secret_manager"
-const secretManagerType = "HASHICORP"
-const secretManagerResourceTypeAndName = secretManagerResourceName + "." + secretManagerType
+const dsfSecretManagerResourceType = "dsfhub_secret_manager"
 
-func TestAccSecretManager_basic(t *testing.T) {
-	log.Printf("======================== BEGIN TEST ========================")
-	log.Printf("[INFO] Running test TestAccSecretManager_basic \n")
+func TestAccDSFSecretManager_Hashicorp(t *testing.T) {	
+	gatewayId := os.Getenv("GATEWAY_ID")
+	if gatewayId == "" {
+		t.Fatal("GATEWAY_ID must be set")
+	}
+
+	const (
+		serverPort = "8200"
+		assetId = testOnPremServerHostName + ":HASHICORP::" + serverPort 
+		resourceName = "example-hashicorp"
+	)
+	
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
-		CheckDestroy: testAccSecretManagerDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckSecretManagerConfigBasic(t),
-				Check: resource.ComposeTestCheckFunc(
-					testCheckSecretManagerExists(secretManagerResourceName),
-					resource.TestCheckResourceAttr(secretManagerResourceTypeAndName, secretManagerResourceName, secretManagerType),
-				),
-			},
-			{
-				ResourceName:      secretManagerResourceTypeAndName,
-				ImportState:       true,
-				ImportStateVerify: true,
-				ImportStateIdFunc: testAccSecretManagerId,
+				Config: testAccDSFSecretManagerConfig_Hashicorp(resourceName, gatewayId, assetId, testOnPremServerHostName, serverPort, "ec2", "vault-role-for-ec2"),
 			},
 		},
 	})
@@ -40,7 +38,7 @@ func TestAccSecretManager_basic(t *testing.T) {
 func testAccSecretManagerId(state *terraform.State) (string, error) {
 	log.Printf("[INFO] Running test testAccSecretManagerId \n")
 	for _, rs := range state.RootModule().Resources {
-		if rs.Type != dsfDataSourceType {
+		if rs.Type != dsfSecretManagerResourceType {
 			continue
 		}
 		return fmt.Sprintf("%s", rs.Primary.ID), nil
@@ -68,26 +66,6 @@ func testCheckSecretManagerExists(secretManagerId string) resource.TestCheckFunc
 	}
 }
 
-func testAccCheckSecretManagerConfigBasic(t *testing.T) string {
-	log.Printf("[INFO] Running test testAccCheckSecretManagerConfigBasic \n")
-	return fmt.Sprintf(`
-resource "%s" "my_test_data_source" {
-	admin_email = "%s"
-	asset_display_name = "%s"
-	asset_id = "%s"
-	gateway_id = "%s""
-	server_host_name = "%s"
-	server_ip = "%s"
-	server_port = "%s"
-	server_type = "%s"
-	sm_connection {
-		reason         = "%s"
-		auth_mechanism = "%s"
-		role_name      = "%s"
-	}
-}`, secretManagerResourceName, testAdminEmail, testAssetDisplayName, testSMAssetId, testGatewayId, testServerHostName, testServerIP, testServerPort, testSMServerType, testSMConnectionReason, testSMAuthMechanism, testSMRoleName)
-}
-
 func testAccSecretManagerDestroy(state *terraform.State) error {
 	log.Printf("[INFO] Running test testAccDSFDataSourceDestroy \n")
 	client := testAccProvider.Meta().(*Client)
@@ -105,4 +83,46 @@ func testAccSecretManagerDestroy(state *terraform.State) error {
 		}
 	}
 	return nil
+}
+
+// Configs
+func testAccSecretManagerConfig_Basic(t *testing.T) string {
+	return fmt.Sprintf(`
+resource "%s" "my_test_data_source" {
+	admin_email = "%s"
+	asset_display_name = "%s"
+	asset_id = "%s"
+	gateway_id = "%s"
+	server_host_name = "%s"
+	server_ip = "%s"
+	server_port = "%s"
+	server_type = "%s"
+	asset_connection {
+		reason         = "%s"
+		auth_mechanism = "%s"
+		role_name      = "%s"
+	}
+}`, dsfSecretManagerResourceType, testAdminEmail, testAssetDisplayName, testSMAssetId, testGatewayId, testServerHostName, testServerIP, testServerPort, testSMServerType, testSMConnectionReason, testSMAuthMechanism, testSMRoleName)
+}
+
+func testAccDSFSecretManagerConfig_Hashicorp(resourceName string, gatewayId string, assetId string, serverHostName string, serverPort string, authMechanism string, roleName string) string {
+	return fmt.Sprintf(`
+resource "` + dsfSecretManagerResourceType + `" "%[1]s" {
+	server_type = "HASHICORP"
+	
+	admin_email = "` + testAdminEmail + `"
+	asset_display_name = "%[3]s"
+	asset_id = "%[3]s"
+	gateway_id = "%[2]s"
+	server_host_name = "%[4]s"
+	server_ip = "%[4]s"
+	server_port = "%[5]s"
+
+	asset_connection {
+		reason         = "default"
+		auth_mechanism = "%[6]s"
+		role_name      = "%[7]s"
+	}
+}`,
+	resourceName, gatewayId, assetId, serverHostName, serverPort, authMechanism, roleName)
 }
