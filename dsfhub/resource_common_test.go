c1f7a8476b3f7ba5fb1773557120326b5ad376c0
mattJsonar <matt.gill@imperva.com>
2024-10-25 07:54:10 -0700

    clarify testAccParseResourceAttributeReference

--- dsfhub/resource_common_test.go
+++ dsfhub/resource_common_test.go
@@ -5,15 +5,20 @@ import (
 	"regexp"
 )
 
-func testAccParseResourceAttributeReference(field string) string {
-	var expr string = `dsfhub_[A-Za-z0-9_-].+\.[A-Za-z0-9_-].+` //e.g. dsfhub_cloud_account.my-cloud-account, dsfhub_cloud_account.my-cloud-account.asset_id
-	var ret string
 
-	isRef, _ := regexp.Match(expr, []byte(field))
-	if isRef {
-		ret = field
+// testAccParseResourceAttributeReference parses a terraform field and 
+// determines whether it is a reference to another resource. If the field is
+// a reference, return the input string and if not, return it wrapped in
+// double-quotes.
+func testAccParseResourceAttributeReference(field string) string {
+	var regExpr string = `dsfhub_[A-Za-z0-9_-].+\.[A-Za-z0-9_-].+` //e.g. dsfhub_cloud_account.my-cloud-account, dsfhub_cloud_account.my-cloud-account.asset_id
+	var parsedField string
+
+	isReference, _ := regexp.Match(regExpr, []byte(field))
+	if isReference {
+		parsedField = field
 	} else {
-		ret = fmt.Sprintf("\"%s\"", field)
+		parsedField = fmt.Sprintf("\"%s\"", field)
 	}
-	return ret
+	return parsedField
 }
