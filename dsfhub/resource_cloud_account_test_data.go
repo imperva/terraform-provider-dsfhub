05d6ea12936adca6f2e68a130cd47a07b363d217
mattJsonar <matt.gill@imperva.com>
2024-11-16 12:34:03 -0800

    cleanup aws cloud account data

--- dsfhub/resource_cloud_account_test_data.go
+++ dsfhub/resource_cloud_account_test_data.go
@@ -2,6 +2,33 @@ package dsfhub
 
 import "fmt"
 
+const awsConnectionDefault = `
+  asset_connection {
+    auth_mechanism  = "default"
+    reason          = "default"
+    region          = "us-west-1"
+  }
+`
+
+const awsConnectionKey = `
+  asset_connection {
+    access_id       = "my-access-id"
+    auth_mechanism  = "key"
+    reason          = "default"
+    region          = "us-east-1"
+    secret_key      = "my-secret-key"
+  }
+`
+
+const awsConnectionProfile = `
+  asset_connection {
+    auth_mechanism  = "profile"
+    reason          = "default"
+    region          = "us-east-2"
+    username        = "dsfhubuser"
+  }
+`
+
 // Output a terraform config for an AWS cloud account resource.
 //
 // Supports all authentication mechanisms: "key", "profile", "iam_role", and
@@ -9,44 +36,24 @@ import "fmt"
 func testAccDSFCloudAccountConfig_Aws(resourceName string, gatewayId string, assetId string, authMechanism string) string {
 	var assetConnectionBlock string
 
-	if authMechanism == "key" {
-		assetConnectionBlock = fmt.Sprintf(`
-      asset_connection {
-        access_id = "my-access-id"
-        auth_mechanism = "` + authMechanism + `"
-        reason = "default"
-        region = "us-east-1"
-        secret_key = "my-secret-key"
-      }
-    `)
-	} else if authMechanism == "profile" {
-		assetConnectionBlock = fmt.Sprintf(`
-      asset_connection {
-        auth_mechanism = "` + authMechanism + `"
-        reason = "default"
-        region = "us-east-2"
-        username = "dsfhubuser"
-      }
-    `)
-	} else {
-		assetConnectionBlock = fmt.Sprintf(`
-      asset_connection {
-        auth_mechanism = "` + authMechanism + `"
-        reason = "default"
-        region = "us-west-1"
-      }
-      `)
-	}
+  switch authMechanism {
+    case "key":
+      assetConnectionBlock = awsConnectionKey
+    case "profile":
+      assetConnectionBlock = awsConnectionProfile
+    default:
+      assetConnectionBlock = awsConnectionDefault
+  }
 
 	return fmt.Sprintf(`
 resource "`+dsfCloudAccountResourceType+`" "%[1]s" {
   server_type = "AWS"
   
-  admin_email = "`+testAdminEmail+`"
-  arn = "%[3]s"
+  admin_email        = "`+testAdminEmail+`"
+  arn                = "%[3]s"
   asset_display_name = "%[3]s"
-  asset_id = "%[3]s"
-  gateway_id = "%[2]s"
+  asset_id           = "%[3]s"
+  gateway_id         = "%[2]s"
 
   `+assetConnectionBlock+`
 }`, resourceName, gatewayId, assetId)
